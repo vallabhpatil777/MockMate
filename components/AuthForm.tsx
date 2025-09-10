9b051ef7ea64543a83402400dfd442da9182a5dd
Vallabh <vallabhpatil777@gmail.com>
2025-09-10 23:23:51 +0100

    setting up and routing file structure

---
+++ components/AuthForm.tsx
@@ -0,0 +1,85 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import logo from '../public/logo.svg'
+import {Form} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { fr } from "zod/locales"
+import Image from "next/image"
+import Link from "next/link"
+import { toast } from "sonner"
+import FormField from "./FormField"
+import { useRouter } from "next/navigation"
+
+
+
+const authFormSchema = (type: FormType) => {
+  
+    return z.object({
+
+        name : type === 'sign-up' ? z.string().min(2) : z.string().optional(),
+        email: z.string().min(5).email(),
+        password: z.string().min(6).max(50)
+    })
+}
+
+const AuthForm = ({type}: {type:FormType}) => {
+    const router = useRouter();
+    const formSchema = authFormSchema(type);
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          name: "",
+          email:"",
+          password: "",
+        },
+      })
+     
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        try{
+
+            if (type === 'sign-in'){
+                console.log('SIGN IN', values)
+                toast.success(`Signed in as ${values.email}`)
+                router.push('/')
+            }
+            else{
+                console.log('SIGN UP', values)
+                toast.success(`Account created for ${values.name}. Please sign in.`)
+                router.push('/sign-in')
+            }
+        } catch(err){
+            console.log(err)
+            toast.error(`Something went wrong:${err}`)
+      }
+    }
+
+  const isSignIn = type === 'sign-in'
+
+  return (
+    <div className="card-border lg:min-w-[566px]">
+        <div className="flex flex-col gap-6 card py-14 px-10">
+            <div className="flex flex-row gap-2 justify-center"><Image src={logo} alt="logo" height={32} width={38}/><h2 className="text-primary-100">MockMate</h2></div>
+            <h3> Practice Job Interview with AI</h3>
+
+
+            <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+        {!isSignIn && <FormField control={form.control} name="name" label="Name" placeholder="Your Name" />}
+        <FormField control={form.control} name="email" label="Email" placeholder="Enter your email" type="email" />
+        <FormField control={form.control} name="password" label="Password" placeholder="Enter your Password" type="password" />
+        <Button className="btn" type="submit">{isSignIn ? 'Sign In' : 'Create an Account'}</Button>
+      </form>
+    </Form>
+     <p className="text-center">
+        {isSignIn ? "Don't have an account? " : "Already have an account? "}
+        <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">{!isSignIn ? "Sign In" : "Sign Up"}</Link>
+     </p>
+    </div>
+    </div>
+  )
+}
+
+export default AuthForm;
