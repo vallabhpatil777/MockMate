bc6d444f6e6a37640fb1d71a3febca2410ccf807
Vallabh <vallabhpatil777@gmail.com>
2025-09-16 00:46:29 +0100

    implemented interview session and feedback page

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,10 +6,21 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
 import { id } from 'zod/locales';
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
 
-const InterviewCard = ({id, userId, role,type,techstack,createdAt} : InterviewCardProps) => {
- const feedback = null as Feedback | null;
- const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
+const InterviewCard = async ({ id,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,} : InterviewCardProps) => {
+  const feedback =
+    userId && id
+      ? await getFeedbackByInterviewId({
+         interviewId: id,
+          userId,
+        })
+      : null; const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
  const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
 
 
--- src/lib/actions/general.action.ts
+++ src/lib/actions/general.action.ts
@@ -42,7 +42,7 @@ export async function getInterviewById(id: string): Promise<Interview | null> {
                 .join("");
           
               const { object : {totalScore, categoryScores, strengths, areasForImprovement, finalAssessment}} = await generateObject({
-                model: google("gemini-2.0-flash-001"),
+                model: google("gemini-2.0-flash"),
                 schema: feedbackSchema,
                 prompt: `
                   You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
