2c1b0c44937b634697f9f1a0a90ad8fe3345832e
Vallabh <vallabhpatil777@gmail.com>
2025-09-16 12:40:55 +0100

    implemented interview session and feedback page

--- src/app/(root)/interview/[id]/page.tsx
+++ src/app/(root)/interview/[id]/page.tsx
@@ -36,7 +36,7 @@ const page = async ({params} : RouteParams)  => {
             <DisplayTechIcons techStack={interview.techstack} />
           </div>
   
-          <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit">
+          <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize">
             {interview.type}
           </p>
         </div>
