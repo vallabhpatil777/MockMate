45430c7625ccd9b656bc9eedab1fbb5a5b50079a
Vallabh <vallabhpatil777@gmail.com>
2025-09-16 18:05:29 +0100

    implemented interview session and feedback page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -86,7 +86,7 @@ const AuthForm = ({type}: {type:FormType}) => {
     <div className="card-border lg:min-w-[566px]">
         <div className="flex flex-col gap-6 card py-14 px-10">
             <div className="flex flex-row gap-2 justify-center"><Image src={logo} alt="logo" height={32} width={38}/><h2 className="text-primary-100">MockMate</h2></div>
-            <h4> Practice Job Interview with AI</h4>
+            <h3> Practice Job Interview with AI</h3>
 
 
             <Form {...form}>
